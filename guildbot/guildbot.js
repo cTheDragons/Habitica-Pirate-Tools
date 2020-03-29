0f8fc4b608b6105aae5ba3768d4802f5f6cb1a16
cTheDragons <cTheDragons@gmail.com>
2020-03-29 21:07:36 +1100

    Adding sample files

---
+++ guildbot/guildbot.js
@@ -0,0 +1,12 @@
+/*
+
+This code is licensed under the same terms as Habitica:
+    https://raw.githubusercontent.com/HabitRPG/habitrpg/develop/LICENSE
+
+
+Contributors:
+    cTheDragons https://github.com/cTheDragons
+
+Currently this is an empty shell that will be added once the code is finished
+
+*/
